265e8dc254bee853dff0d6837087468fe797fc94
Dimitar Stoichev <dstoichev@users.noreply.github.com>
2018-01-18 16:06:12 +0000

    Output names of current open documents

--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
-alert('Hello World');
+var docs = app.documents,
+    alertText = ''.concat('Current open documents', "\n");
+    
+for (var i = 0; i < docs.length; i++)
+{
+    alertText = ''.concat(alertText, docs[i].name, "\n");
+}
+
+alert(alertText);
