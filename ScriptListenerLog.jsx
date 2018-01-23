f57eaca47b138dd171c8c7579825f44c670d26da
Dimitar Stoichev <dstoichev@users.noreply.github.com>
2018-01-23 13:23:32 +0000

    Unsuccessful Add Mask to Selection

--- ScriptListenerLog.jsx
+++ ScriptListenerLog.jsx
@@ -35,3 +35,35 @@ executeAction( idInvs, undefined, DialogModes.NO );
 var idCpyM = charIDToTypeID( "CpyM" );
 executeAction( idCpyM, undefined, DialogModes.NO );
 
+/////////////////////////////////////////////////////////////////////
+
+// =======================================================
+var idAdd = charIDToTypeID( "Add " );
+    var desc3 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref2 = new ActionReference();
+        var idChnl = charIDToTypeID( "Chnl" );
+        var idChnl = charIDToTypeID( "Chnl" );
+        var idMsk = charIDToTypeID( "Msk " );
+        ref2.putEnumerated( idChnl, idChnl, idMsk );
+    desc3.putReference( idnull, ref2 );
+    var idT = charIDToTypeID( "T   " );
+        var ref3 = new ActionReference();
+        var idChnl = charIDToTypeID( "Chnl" );
+        var idfsel = charIDToTypeID( "fsel" );
+        ref3.putProperty( idChnl, idfsel );
+    desc3.putReference( idT, ref3 );
+    var idVrsn = charIDToTypeID( "Vrsn" );
+    desc3.putInteger( idVrsn, 1 );
+    var idmaskParameters = stringIDToTypeID( "maskParameters" );
+    desc3.putBoolean( idmaskParameters, true );
+executeAction( idAdd, desc3, DialogModes.NO );
+
+// =======================================================
+var idInvs = charIDToTypeID( "Invs" );
+executeAction( idInvs, undefined, DialogModes.NO );
+
+// =======================================================
+var idCpyM = charIDToTypeID( "CpyM" );
+executeAction( idCpyM, undefined, DialogModes.NO );
+
--- main.jsx
+++ main.jsx
@@ -1,25 +1,65 @@
 #include "./include/json2.js"
 
-var savePath = '~/PS Dev/Action Target';
+var savePath = '~/PS Dev/Action Target',
+    cropLayerName = 'CROP';
 
 function main()
 {
     var docs = app.documents,
         alertText = ''.concat('Current open documents', "\n"),
-        test = {"a": "alabala", "bool": true},
-        doc;
-        
-    alertText += JSON.stringify(test) + "\n";
+        doc, cropLayerReference, corner;
         
     for (var i = 0; i < docs.length; i++)
     {
         doc = docs[i];
         app.activeDocument = doc;
-        alertText = ''.concat(alertText, doc.name, "\n");
+        cropLayerReference = doc.artLayers.getByName(cropLayerName);
+        doc.activeLayer = cropLayerReference;
+        
+        addMaskToSelection();
+        /*
+        doc.selection.select(cropLayerReference.bounds, Selection.EXTEND);
+        doc.selection.invert();
+        doc.selection.copy(true);
+        */
+        for (var j = 0; j < 4; j++)
+        {
+            corner = doc.selection.bounds[j];
+            alertText = ''.concat(alertText, corner[0], ' ', corner[1],  "\n");
+        }
+        alertText = ''.concat(alertText, doc.name, "\n\n");
         //saveJpeg(doc);
     }
     
     alert(alertText);    
+}
+
+function addMaskToSelection()
+{
+    var idAdd = charIDToTypeID( "Add " ),
+        desc3 = new ActionDescriptor(),
+        idnull = charIDToTypeID( "null" ),
+        ref2 = new ActionReference(),
+        idChnl = charIDToTypeID( "Chnl" ),
+        idMsk = charIDToTypeID( "Msk " );
+    
+    ref2.putEnumerated( idChnl, idChnl, idMsk );
+    desc3.putReference( idnull, ref2 );
+    
+    var idT = charIDToTypeID( "T   " ),
+        ref3 = new ActionReference(),
+        idfsel = charIDToTypeID( "fsel" );
+    
+    ref3.putProperty( idChnl, idfsel );
+    desc3.putReference( idT, ref3 );
+    
+    var idVrsn = charIDToTypeID( "Vrsn" );
+    desc3.putInteger( idVrsn, 1 );
+    
+    var idmaskParameters = stringIDToTypeID( "maskParameters" );
+    desc3.putBoolean( idmaskParameters, true );
+    
+    executeAction( idAdd, desc3, DialogModes.NO );
 }
 
 function saveJpeg(doc)
