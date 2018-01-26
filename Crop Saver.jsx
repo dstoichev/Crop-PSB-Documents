d2d2f090cb2ad5e29463abfb3c2f039a80aa261d
Dimitar Stoichev <dimitarstoichev@artray.co.uk>
2018-01-26 17:32:59 +0200

    More fixing has CROP layer validation

--- Crop Saver.jsx
+++ Crop Saver.jsx
@@ -62,6 +62,7 @@
                     cropLayerRef = doc.artLayers.getByName(this.cropLayerName);
                 } catch(e) {                    
                     if ('No such element' == e.message || '- The object "layer "CROP"" is not currently available.' == e.message) {
+                        // test both
                         alertText = ''.concat(alertText, doc.name, "\n"," Warning: The 'CROP' layer is missing.", "\n", e.message, "\n\n");
                         continue;
                     }
